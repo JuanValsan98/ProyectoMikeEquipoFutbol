8103f0653ab0ed282b3ea5ce4a8feef5067ef728
JuanValsan98 <jdvalsan08@gmail.com>
2026-09-14 16:43:17 -0500

    Nuevo cambio, creacion de service para rendimiento y creacion de Controller para rendimiento

---
+++ src/main/java/com/entrenamiento/demo/Controller/rendimientoController.java
@@ -0,0 +1,5 @@
+package com.entrenamiento.demo.Controller;
+
+public class rendimientoController {
+    
+}
--- src/main/java/com/entrenamiento/demo/service/jugadorService.java
+++ src/main/java/com/entrenamiento/demo/service/jugadorService.java
@@ -1,5 +1,21 @@
 package com.entrenamiento.demo.service;
 
+import org.springframework.stereotype.Service;
+
+import com.entrenamiento.demo.dto.jugadorResponseDTO;
+import com.entrenamiento.demo.dto.jugadorResponseDTO;
+import com.entrenamiento.demo.dto.rendimientojugadorRequestDTO;
+import com.entrenamiento.demo.dto.rendimientojugadorResponseDTO;
+import com.entrenamiento.demo.Entity.jugador;
+import com.entrenamiento.demo.repository.jugadorRepository;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+@Service 
+@AllArgsConstructor 
+@Data 
+
 public class servicejugador {
+    private final jugadorRepository JugadorRepository;  
     
 }
