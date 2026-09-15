2f0759447ac5cd7fbf660b652905b7c82eb1bbbe
JuanValsan98 <jdvalsan08@gmail.com>
2026-09-15 17:10:06 -0500

    Intento de crear el rendimiento del jugador en rendimientoService y tambien la configuracion de rendimientoRepository

--- src/main/java/com/entrenamiento/demo/service/rendimientoService.java
+++ src/main/java/com/entrenamiento/demo/service/rendimientoService.java
@@ -1,5 +1,55 @@
 package com.entrenamiento.demo.service;
 
+import com.entrenamiento.demo.dto.rendimientojugadorRequestDTO;
+import com.entrenamiento.demo.dto.rendimientojugadorResponseDTO;
+
+import org.springframework.stereotype.Service;
+import java.util.Optional;
+
+import com.entrenamiento.demo.Entity.jugador;
+import com.entrenamiento.demo.Entity.rendimientojugador;
+import com.entrenamiento.demo.repository.rendimientoRepository;
+import com.entrenamiento.demo.repository.jugadorRepository;
+
+import lombok.AllArgsConstructor;
+
+
+@Service 
+@AllArgsConstructor 
 public class rendimientoService {
+    private final jugadorRepository jugadorRepository;
+    private final rendimientoRepository rendimientoRepository;
+
+    public rendimientoJugadorResponseDTO crearRendimiento(rendimientojugadorRequestDTO request){
+          Optional<jugador> jugadorEncontrado = jugadorRepository.findById(request.getJugadorId());
+
+if (jugadorEncontrado.isEmpty()) {
+    throw new RuntimeException("Ese jugador con esa id no existe");
+}
+
+jugador Jugador = jugadorEncontrado.get();
+
+double resultado = (request.getPotenciaDeTiro() * 0.20)
+                 + (request.getVelocidad() * 0.30)
+                 + (request.getPases() * 0.50);
+
+    rendimientojugador rendimientojugador = new rendimientojugador();
+    rendimientojugador.setPotenciaDeTiro(request.getPotenciaDeTiro());
+    rendimientojugador.setVelocidad(request.getVelocidad());
+    rendimientojugador.setPases(request.getPases());
+    rendimientojugador.setresultado(request.getresultado());
+    rendimientoRepository.save(rendimientojugador);
+
+    rendimientojugadorResponseDTO response = new rendimientojugadorResponseDTO();
+    rendimientojugador.setJugador(jugador);
+    response.setPotenciaDeTiro(rendimientojugador.getPotenciaDeTiro());
+    response.setVelocidad(rendimientojugador.getVelocidad());
+    response.setPases(rendimientojugador.getPases());
+    response.setresultado(rendimientojugador.getresultado());
+    return response;
+
+    }
+
+
     
 }
