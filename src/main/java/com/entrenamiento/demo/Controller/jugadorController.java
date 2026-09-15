ab64d9d1a129b0ff6c86512847c04592af442e9f
JuanValsan98 <jdvalsan08@gmail.com>
2026-09-14 23:50:57 -0500

    Creacion de Run point para crear a los usuarios

--- src/main/java/com/entrenamiento/demo/Controller/jugadorController.java
+++ src/main/java/com/entrenamiento/demo/Controller/jugadorController.java
@@ -1,6 +1,27 @@
 package com.entrenamiento.demo.Controller;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.entrenamiento.demo.dto.jugadorRequestDTO;
+import com.entrenamiento.demo.dto.jugadorResponseDTO;
 import com.entrenamiento.demo.dto.rendimientojugadorRequestDTO;
+import com.entrenamiento.demo.service.jugadorService;
+import lombok.AllArgsConstructor;
+
+
+@RestController 
+@AllArgsConstructor 
+@RequestMapping("/jugadorController")
 public class jugadorController {
+    public final jugadorService JugadorService;
+    @PostMapping("/createjugador")
+   public ResponseEntity<jugadorResponseDTO> createJugador (@RequestBody jugadorRequestDTO request){
+    jugadorResponseDTO response = JugadorService.createJugador(request);
+    return ResponseEntity.status(HttpStatus.CREATED).body(response);
     
+}
 }
--- src/main/java/com/entrenamiento/demo/service/jugadorService.java
+++ src/main/java/com/entrenamiento/demo/service/jugadorService.java
@@ -2,6 +2,7 @@ package com.entrenamiento.demo.service;
 
 import org.springframework.stereotype.Service;
 
+import com.entrenamiento.demo.dto.jugadorRequestDTO;
 import com.entrenamiento.demo.dto.jugadorResponseDTO;
 import com.entrenamiento.demo.dto.jugadorResponseDTO;
 import com.entrenamiento.demo.dto.rendimientojugadorRequestDTO;
@@ -15,7 +16,23 @@ import lombok.Data;
 @AllArgsConstructor 
 @Data 
 
-public class servicejugador {
+public class jugadorService {
     private final jugadorRepository JugadorRepository;  
+
+    public jugadorResponseDTO createJugador (jugadorRequestDTO request) {
+        
+       
+        public JugadorResponseDTO crearJugador(jugadorRequestDTO request) {
+        jugador Jugador = new Jugador();
+        jugador.setname(request.getname());
+        jugadorRepository.save(jugador);
+
+        jugadorResponseDTO response = new JugadorResponseDTO();
+        response.setid(jugador.getid());
+        response.setname(jugador.getname());
+        return response;
+    }
     
+}
+
 }
